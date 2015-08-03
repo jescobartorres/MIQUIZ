d9ce807743aca1dd74150623347d2d3553534a4c
jescobartorres <jescobartorres@gmail.com>
2015-08-03 18:11:11 +0200

    primera pagina y favicon

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+// no usado en QUIZ
 app.use('/users', users);
 
 // catch 404 and forward to error handler
