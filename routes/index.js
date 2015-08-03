28783e66b48bf35da940f2a9f5c8a219650f4d7f
jescobartorres <jescobartorres@gmail.com>
2015-08-03 22:48:03 +0200

    Listas y mantenimiento

--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,30 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Mi Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); //autoload : commentId
+
+// Definición de rutas de /quizes
 
 // GET QUIZES/QUESTION
-router.get('/quizes/question', quizController.question);
+//router.get('/quizes/question', quizController.question);
 // GET QUIZES/ANSWER
-router.get('/quizes/answer', quizController.answer);
+//router.get('/quizes/answer', quizController.answer);
+
+router.get('/quizes',                       quizController.index);
+router.get('/quizes/:quizId(\\d+)',         quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
+router.get('/quizes/new',                   quizController.new);
+router.post('/quizes/create',               quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',    quizController.edit);
+router.put('/quizes/:quizId(\\d+)',         quizController.update);
+router.delete('/quizes/:quizId(\\d+)',      quizController.destroy);
 
 
-/* GET author */
+// GET author
 router.get('/author', function (req, res) {
     res.render('author', { errors: [] });
 });
-
 
 module.exports = router;
